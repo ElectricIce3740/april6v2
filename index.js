7bc830b3239200fac13af7364c90bad415714dc2
ElectricIce3740 <c7m.moore@gmail.com>
2025-04-07 20:29:50 +0000

    hey

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000; // Use environment port for hosting, default to 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hey!');
 });
 
 app.get('/api/data', (req, res) => {
