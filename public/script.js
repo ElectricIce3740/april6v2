8cf4c83df48c99fdbcfaf0d318384aafec05444b
ElectricIce3740 <c7m.moore@gmail.com>
2025-04-13 00:30:13 +0000

    final weather

--- public/script.js
+++ public/script.js
@@ -1,11 +1,9 @@
-async function getData() {
-    const url = "https://api.open-meteo.com/v1/forecast?latitude=36.5484&longitude=-82.5618&daily=temperature_2m_max,temperature_2m_min&timezone=auto&forecast_days=1&temperature_unit=fahrenheit";
+async function getData(url) {
     try {
-      const response = await fetch(url);
+      const response = await fetch(url)
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
       }
-  
       const datajson = await response.json();
       console.log(datajson);
       if (typeof datajson === 'object') {
@@ -20,7 +18,7 @@ async function getData() {
   }
 
 async function displayData() {
-    const replyans = await getData();
+    const replyans = await getData("https://api.open-meteo.com/v1/forecast?latitude=36.5484&longitude=-82.5618&daily=temperature_2m_max,temperature_2m_min&timezone=auto&forecast_days=1&temperature_unit=fahrenheit");
     const obj = JSON.parse(replyans);
     const tempMax = await obj.daily.temperature_2m_max[0];
     const tempMin = await obj.daily.temperature_2m_min[0];
